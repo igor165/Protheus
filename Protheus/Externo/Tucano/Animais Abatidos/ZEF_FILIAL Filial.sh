6782d4f4610de567403f9d9400a794ac60f4917f
igor165 <85706530+igor165@users.noreply.github.com>
2024-07-11 17:41:37 -0300

    LVFATI01

---
+++ Protheus/Externo/Tucano/Animais Abatidos/ZEF_FILIAL Filial.sh
@@ -0,0 +1,32 @@
+ZEF_FILIAL	Filial      
+ZEF_NUMANI	Qtd Animais 
+ZEF_PERIOD	Periodo Conf
+ZEF_PESOKG	Peso Ini KG 
+ZEF_PESOAR	Peso Ini @      // VISUAL
+ZEF_GMD   	GMD Estimado    //  aumentar 4 casas decimais
+ZEF_RENDIM	Carcaça %   
+ZEF_PESOF 	Peso Final      // VISUAL 
+ZEF_PESOFC	Carc. Pronta    // VISUAL
+ZEF_QTDARR	Qtd @ Coloca    // VISUAL
+ZEF_CUSTOA	Custo @ Prod    // VISUAL
+ZEF_VLRVEN	Vlr @ Venda 
+ZEF_VLRENT	Vlr @ Entrad
+ZEF_CUSCAB	Custo Cabeca    // VISUAL -- PEGAR DA GRID
+ZEF_COPCAB	Custo Op Dia
+ZEF_CUSOPA	Op+Alimentac    // VISUAL 
+ZEF_VLRCEN	Carcaça Entr    // VISUAL
+ZEF_VLRALI	Valor Alimen    // VISUAL -- ZEF_PERIOD x PEGAR DA GRID
+ZEF_RECEIT	Receita Brut       //VISUAL
+ZEF_DESTOT	Despesa Tot     //VISUAL
+ZEF_RECLIQ	Receita Liq     //VISUAL
+ZEF_RECTOT	Receita Tot     //VISUAL
+ZEG_FILIAL	Filial      
+ZEG_CODIGO	Codigo      
+ZEG_ITEM  	Item        
+ZEG_PRODUT	Produto     
+ZEG_DESC  	Produto          VISUAL
+ZEG_KGDIA 	Kg/Cab/Dia      // aumentar 4 casas decimais
+ZEG_KGTOTA	Kg Total    //VISUAL 
+ZEG_VLRUNI	Vlr Unitario    // aumentar 4 casas decimais
+ZEG_VLRDIA  	Vlr Dia        // VISUAL  aumentar 4 casas decimais
+ZEG_VLRTOT	Vlr Total       //VISUAL
