43d4b48247c7cd966cfcb2cb7d3bfb3d3c619ec2
igor165 <85706530+igor165@users.noreply.github.com>
2024-08-30 20:11:18 -0300

    windowsform

---
+++ WindowsForm/Projeto_03/Projeto_03/Form1.cs
@@ -0,0 +1,25 @@
+namespace Projeto_03
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Teste");
+        }
+    }
+}
